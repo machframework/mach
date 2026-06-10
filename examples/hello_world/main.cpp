2efae8e73356682bb2ae8509faea62cf23beef01
asaf08da <asaf10dahan@gmail.com>
2026-06-10 12:03:47 +0300

    docs: add route registration example with a valid handler in hello_world example

--- examples/hello_world/main.cpp
+++ examples/hello_world/main.cpp
@@ -2,13 +2,16 @@
 #include <thread>
 
 #include <mach/App.hpp>
+#include <mach/Context.hpp>
 
 int main() {
 	int threads = std::thread::hardware_concurrency();
 
 	auto app = mach::App("127.0.0.1", 3143, threads);
 
-	app.addRoute(mach::http::Method::Get, "/users", nullptr);
+	app.addRoute(mach::http::Method::Get, "/users", [](mach::Context& context) {
+		std::cout << "Request Body: " << context.request.body() << std::endl;
+	});
 
 	app.run();
 }
