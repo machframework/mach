c96966153c7a1d86b625de25dd647098b4f9c577
asaf08da <asaf10dahan@gmail.com>
2026-06-20 12:34:54 +0300

    refactor: move dependency type validation to Container

--- include/mach/detail/di/Container.hpp
+++ include/mach/detail/di/Container.hpp
@@ -25,10 +25,25 @@ namespace mach::detail::di
 	private:
 		std::unordered_map<std::type_index, ServiceDescriptor> m_serviceRegistry;
         std::unordered_map<std::type_index, std::shared_ptr<void>> m_singletonInstances;
+
+        template <typename T>
+        static constexpr bool isValidServiceType =
+            std::is_class_v<T> &&
+            !std::is_const_v<T> &&
+            !std::is_reference_v<T> &&
+            !std::is_pointer_v<T> &&
+            !std::is_same_v<T, std::string> &&
+            !std::is_same_v<T, std::string_view>;
 	};
 
     template <typename T, typename... Deps>
     void Container::addService(ServiceLifetime lifetime) {
+        static_assert(isValidServiceType<T>,
+            "Service type must be a non-const, non-reference, non-pointer class type.");
+
+        static_assert((isValidServiceType<Deps> && ...),
+            "Dependency types must be non-const, non-reference, non-pointer class types.");
+
         ServiceDescriptor descriptor{
             .type = typeid(T),
             .lifetime = lifetime,
