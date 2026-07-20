0b94fa49b6559b63d5995ac120bddcf2fc09a371
asaf08da <asaf10dahan@gmail.com>
2026-07-20 16:46:37 +0300

    chore: separate public Next class into declaration and implementation files

--- include/mach/middleware/Next.hpp
+++ include/mach/middleware/Next.hpp
@@ -11,25 +11,12 @@ namespace mach
 	class Next {
 
 	public:
-		Next(
-			std::function<void()> next
-		)
-			: m_next(std::move(next))
-		{ }
+		Next(std::function<void()> next);
 
 		Next(const Next&) = delete;
 		Next& operator=(const Next&) = delete;
 
-		void operator()() {
-			if (m_invoked) {
-				throw std::logic_error(
-					"Mach error: next() may only be invoked once per middleware execution."
-				);
-			}
-
-			m_invoked = true;
-			m_next();
-		}
+		void operator()();
 
 	private:
 		std::function<void()> m_next;
