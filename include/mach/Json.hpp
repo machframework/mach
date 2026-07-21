8f5ddc6448559b0720967d41f28839baed33a32e
Asaf08da <asaf10dahan@gmail.com>
2026-07-21 19:16:04 +0300

    docs: document the json binding macro including the warning regarding default values

--- include/mach/Json.hpp
+++ include/mach/Json.hpp
@@ -7,5 +7,15 @@ namespace mach
 	using Json = nlohmann::json;
 }
 
+/**
+ * Defines JSON serialization and deserialization functions for a type using
+ * nlohmann::json.
+ *
+ * @warning During deserialization, all listed fields are required by default.
+ *			C++ default member initializers are not applied to omitted JSON properties.
+ *
+ * @param Type The type to serialize.
+ * @param ...  The members to serialize and deserialize.
+ */
 #define MACH_DEFINE_JSON(Type, ...) \
     NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(Type, __VA_ARGS__)
