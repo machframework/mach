eabfe267e5961cc97af4789bce79d33a25d55f4b
asaf08da <asaf10dahan@gmail.com>
2026-07-22 13:23:09 +0300

    test: add move-only object binding tests

---
+++ tests/hardening/binding/MoveOnlyTypeBindingTests.cpp
@@ -0,0 +1,40 @@
+#include <cassert>
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <mach/Json.hpp>
+
+#include <mach/detail/binding/BodyBinder.hpp>
+
+struct MoveOnlyDto
+{
+    std::string name;
+    int age;
+
+    MoveOnlyDto() = default;
+
+    MoveOnlyDto(const MoveOnlyDto&) = delete;
+    MoveOnlyDto& operator=(const MoveOnlyDto&) = delete;
+
+    MoveOnlyDto(MoveOnlyDto&&) = default;
+    MoveOnlyDto& operator=(MoveOnlyDto&&) = default;
+};
+
+MACH_DEFINE_JSON(MoveOnlyDto, name, age);
+
+int main() {
+    auto binder = mach::detail::binding::BodyBinder();
+
+    const auto body = R"(
+    {
+        "name": "Asaf",
+        "age": 18
+    }
+    )";
+
+    auto dto = binder.bind<MoveOnlyDto>(body);
+
+    assert(dto.name == "Asaf");
+    assert(dto.age == 18);
+}
