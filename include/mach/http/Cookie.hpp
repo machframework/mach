f9b9f44cefd9c12a55b3031858b762aea476a22c
Asaf08da <asaf10dahan@gmail.com>
2026-08-04 18:46:24 +0300

    feat: create a Cookie abstraction

---
+++ include/mach/http/Cookie.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <optional>
+
+namespace mach
+{
+    enum class SameSite {
+        Strict,
+        Lax,
+        None
+    };
+
+    struct Cookie {
+        std::string name;
+        std::string value;
+
+        std::string path = "/";
+        std::optional<std::string> domain;
+
+        std::optional<std::chrono::system_clock::time_point> expires;
+        std::optional<std::chrono::seconds> maxAge;
+
+        SameSite sameSite = SameSite::Lax;
+
+        bool secure = false;
+        bool httpOnly = false;
+    };
+}
---
+++ src/http/Headers.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace mach::detail::http::headers
+{
+    inline constexpr std::string_view SetCookie = "set-cookie";
+    inline constexpr std::string_view ContentType = "content-type";
+    inline constexpr std::string_view ContentLength = "content-length";
+}
