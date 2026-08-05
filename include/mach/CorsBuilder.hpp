15f093980cad6c46ada9a0cc9f7528abcfddd490
asaf08da <asaf10dahan@gmail.com>
2026-08-05 15:39:25 +0300

    feat: add CorsBuilder class for configuring CORS policies

---
+++ include/mach/CorsBuilder.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <initializer_list>
+#include <string_view>
+
+#include <mach/http/Method.hpp>
+
+#include <mach/detail/cors/CorsOptions.hpp>
+
+namespace mach
+{
+    class CorsBuilder {
+
+    public:
+        CorsBuilder() = default;
+
+        CorsBuilder(const CorsBuilder&) = delete;
+        CorsBuilder& operator=(const CorsBuilder&) = delete;
+
+        CorsBuilder(CorsBuilder&&) noexcept = default;
+        CorsBuilder& operator=(CorsBuilder&&) noexcept = default;
+
+        ~CorsBuilder() = default;
+
+        CorsBuilder& allowOrigin(std::string_view origin);
+        CorsBuilder& allowOrigins(std::initializer_list<std::string_view> origins);
+
+        CorsBuilder& allowMethod(http::Method method);
+        CorsBuilder& allowMethods(std::initializer_list<http::Method> methods);
+
+        CorsBuilder& allowHeader(std::string_view header);
+        CorsBuilder& allowHeaders(std::initializer_list<std::string_view> headers);
+
+        CorsBuilder& exposeHeader(std::string_view header);
+        CorsBuilder& exposeHeaders(std::initializer_list<std::string_view> headers);
+
+        CorsBuilder& allowAnyOrigin();
+        CorsBuilder& allowAnyHeader();
+        CorsBuilder& allowAnyMethod();
+
+        CorsBuilder& allowCredentials(bool enabled = true);
+        CorsBuilder& maxAge(std::chrono::seconds maxAge);
+
+    private:
+        detail::cors::CorsOptions m_options;
+    };
+}
---
+++ src/cors/CorsBuilder.cpp
@@ -0,0 +1,84 @@
+#include <mach/CorsBuilder.hpp>
+
+namespace mach
+{
+    CorsBuilder& CorsBuilder::allowOrigin(std::string_view origin){
+        m_options.allowedOrigins.emplace(origin);
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowOrigins(std::initializer_list<std::string_view> origins) {
+        for (auto origin : origins) {
+            this->allowOrigin(origin);
+        }
+
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowMethod(http::Method method) {
+        m_options.allowedMethods.emplace(method);
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowMethods(std::initializer_list<http::Method> methods) {
+        for (auto method : methods) {
+            this->allowMethod(method);
+        }
+
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowHeader(std::string_view header){
+        m_options.allowedHeaders.emplace(header);
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowHeaders(std::initializer_list<std::string_view> headers) {
+        for (auto header : headers) {
+            this->allowHeader(header);
+        }
+
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::exposeHeader(std::string_view header) {
+        m_options.exposedHeaders.emplace(header);
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::exposeHeaders(std::initializer_list<std::string_view> headers) {
+        for (auto header : headers) {
+            this->exposeHeader(header);
+        }
+
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowAnyOrigin() {
+        m_options.allowAnyOrigin = true;
+        m_options.allowedOrigins.clear();
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowAnyHeader() {
+        m_options.allowAnyHeader= true;
+        m_options.allowedHeaders.clear();
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowAnyMethod() {
+        m_options.allowAnyMethod = true;
+        m_options.allowedMethods.clear();
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::allowCredentials(bool enabled) {
+        m_options.allowCredentials = enabled;
+        return *this;
+    }
+
+    CorsBuilder& CorsBuilder::maxAge(std::chrono::seconds maxAge) {
+        m_options.maxAge = maxAge;
+        return *this;
+    }
+}
