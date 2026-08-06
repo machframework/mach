5f8b28024de0af01f96152e4889e9dbab7bb1ac3
asaf08da <asaf10dahan@gmail.com>
2026-08-06 21:27:31 +0300

    docs: add documentation to 'CsrfBuilder' class

--- include/mach/CsrfBuilder.hpp
+++ include/mach/CsrfBuilder.hpp
@@ -8,6 +8,17 @@ namespace mach
 {
     class AppBuilder;
 
+    /**
+     * Configures the application's Cross-Site Request Forgery (CSRF) protection.
+     *
+     * Provides a fluent interface for configuring the cookie and request header
+     * used by the CSRF middleware.
+     *
+     * Instances of this class are intended to be configured through
+     * `AppBuilder::addCsrf` and are not typically created directly.
+     *
+     * @thread_safety This class is not thread-safe.
+     */
     class CsrfBuilder {
 
     public:
@@ -19,8 +30,26 @@ namespace mach
 
         ~CsrfBuilder() = default;
 
+        /**
+         * Sets the name of the cookie used to store the CSRF token.
+         *
+         * @param name Cookie name.
+         *
+         * @return A reference to this builder, allowing chaining.
+         *
+         * @thread_safety This function is not thread-safe.
+         */
         CsrfBuilder& cookieName(std::string_view name);
 
+        /**
+         * Sets the name of the request header used to submit the CSRF token.
+         *
+         * @param name Header name.
+         *
+         * @return A reference to this builder, allowing chaining.
+         *
+         * @thread_safety This function is not thread-safe.
+         */
         CsrfBuilder& headerName(std::string_view name);
 
     private:
